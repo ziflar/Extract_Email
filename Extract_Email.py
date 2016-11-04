883f55ae7f8f1c4dcb69293d15516eb29c252a52
ziflar <zembla2009@gmail.com>
2016-11-04 09:54:12 +0000

    Delete Extract_Email.py

--- Extract_Email.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import re, sys
-email_pattern = re.compile('([.|\w]+@(\w+[.]\w+[.|\w+])\w+)')
-data = sys.stdin.read()
-matches = re.findall(email_pattern, data)
-emails = [match[0] for match in matches]
-unique_emails = list(set(emails))
-print ', '.join(unique_emails)
